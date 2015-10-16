0cc730147fd5bb72e655dc964fd3f56199cb6efa
ugliestcerberus <Guest@WTS1.attlocal.net>
2015-10-15 18:28:35 -0700

    for loops and learn about using if statements

---
+++ src/SpudCounter.java
@@ -0,0 +1,15 @@
+public class SpudCounter {
+	public static void main(String[] args) {
+		for (int i = 1; i < 8; i++) {
+
+			if (i == 4) {
+				System.out.println("4");
+			} else {
+				System.out.println(i + "potato");
+			}
+
+		}
+		System.out.println("more");
+
+	}
+}
---
+++ src/bieferfever.java
@@ -0,0 +1,23 @@
+
+public class bieferfever {
+public static void main(String[] args) {
+	for (int j = 0; j < 2; j++) {
+		
+		
+		for (int i = 0; i < 3; i++) {
+		System.out.println("baby");
+		
+	}
+		if (j==0) {
+			System.out.println("Oh");
+			
+		}
+
+	if (j==1) {
+		System.out.println("Oh");
+		System.out.println("I thought you'd always be mine");
+	}
+
+	
+	}}
+}
