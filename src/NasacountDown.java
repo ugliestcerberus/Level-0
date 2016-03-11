2e1d0cd203c9d6cf6d6fb0b674e5792115e16d30
Revanty <league@WTS14.attlocal.net>
2016-03-10 18:29:27 -0800

    the

---
+++ src/NasacountDown.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+// Copyright Wintriss Technical Schools 2013
+/**
+ * Kata: NASA Countdown
+ * 
+ * The NASA wants to shoot yet another rocket to the orbit. As this is your first day as a wannabe scientist at the
+ * ground control, you are very excited. So excited that when the gal responsible for the countdown asks you to bring
+ * her some coffee, you accidentally spill it all over her lap and the computer – both are not capable of doing their
+ * job now.
+ * 
+ * But the liftoff is only seconds away. Somebody has to do the countdown – or the rocket can’t start and millions of
+ * dollars are lost! In a moment of great bravery you remember you learned how to count (a long time ago in High
+ * School). So you take the chance!
+ * 
+ * The ground control chief will tell you where to start – then count to 0. (example for ’11’: 11 10 9 8 7 6 5 4 3 2 1
+ * 0)
+ **/
+public class NasacountDown {
+	public static void main(String[] args) {
+		String Time = JOptionPane.showInputDialog("When do you want to start?");
+		int timer = Integer.parseInt(Time);
+		System.out.println(timer);
+		for (int i = timer; i > 0; i--) {
+			System.out.println("Blast Off in " + i);
+		}
+		System.out.println("Rocket is blasting off!");
+
+	}
+
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/OddlyLoopy.java
@@ -0,0 +1,8 @@
+
+public class OddlyLoopy {
+	public static void main(String[] args) {
+		for (int i = 1; i < 500; i += 2) {
+			System.out.println(i);
+		}
+	}
+}
